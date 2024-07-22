b3426ecbd4b6b34ab319c18ca8a0d25f3d08b419
Siddharth Rao <siddrao.926@gmail.com>
2024-07-22 10:02:26 -0400

    Added Tests In Main Using The GC

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+#include "garbagecollector.h"
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  // Instantiating Garbage Collector
+  GarbageCollector gc;
+
+  // Allocation Of Root Object
+  Object* root = gc.allocate();
+
+  // Allocation Of Child Refrences
+  Object* refrence1 = gc.allocate();
+  Object* refrence2 = gc.allocate();
+
+  // Set Up Refrences
+  root->addReference(refrence1);
+  root->addReference(refrence2);
+
+  // Simulate some objects becoming unreachable
+  refrence1 = nullptr;
+  refrence2 = nullptr;
+
+  // Preform Garbage Collection
+  gc.markAll(root);
+  gc.sweep();
+
   return 0;
 }
