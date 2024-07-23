5bfbba6edefca748e83b9531c6a32a84a15b74df
Siddharth Rao <siddrao.926@gmail.com>
2024-07-23 13:32:18 -0400

    Refactored Analyze Code Script

--- analyze_code.sh
+++ analyze_code.sh
@@ -5,7 +5,7 @@ echo "clang-tidy"
 echo "====================================================="
 
 if hash clang-tidy 2>/dev/null; then
-  clang-tidy ./src/*.cpp -- -std=c++14
+  clang-tidy ./src/*.cpp --
 else
   echo "WARNING: clang-tidy not available."
 fi
@@ -16,9 +16,19 @@ echo "clang-format"
 echo "====================================================="
 
 if hash clang-format 2>/dev/null; then
-  for f in ./src/*.cpp; do
+  for f in ./src/*.{cpp,h}; do
     echo "Running clang-format on $f"
-    clang-format $f | diff $f -
+
+    tmp_file=$(mktemp)
+    
+    clang-format -style=file $f > "$tmp_file"
+
+    echo "Diff for $f:"
+    diff -u $f "$tmp_file"
+
+    rm "$tmp_file"
+    
+    echo ""
   done
 else
   echo "WARNING: clang-format not available"
